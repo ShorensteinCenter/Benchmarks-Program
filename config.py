8a48269c937cf514d484f5bedc380ebffc960db1
William Hakim <williamhakim10@gmail.com>
2018-02-23 17:32:21 -0800

    permissions for database

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'test_secret_key'
 	CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672/'
 	SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
-		'sqlite:///' + os.path.join(basedir, 'app.db'))
+		'sqlite:///' + os.path.join(basedir, 'db/app.db'))
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = 465
