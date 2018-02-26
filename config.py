395ddb95473ac46370e50ecb9a6fb3349fb6f43e
William Hakim <williamhakim10@gmail.com>
2018-02-26 15:48:51 -0800

    set server_name in config

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config(object):
 	SQLALCHEMY_DATABASE_URI = (os.environ.get('DATABASE_URL') or
 		'sqlite:///' + os.path.join(basedir, 'app.db'))
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	SERVER_NAME = '34.218.1.48'
 	MAIL_SERVER = 'smtp.gmail.com'
 	MAIL_PORT = 465
 	MAIL_USE_SSL = True
