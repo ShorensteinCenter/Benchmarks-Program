6b34e7043c97669628a4bdaeecc1ec1ad98b6066
William Hakim <williamhakim10@gmail.com>
2019-01-14 15:20:01 -0800

    modified celery beat tasks for background data updating and monthly email reports, wrote unit tests

--- app/tasks.py
+++ app/tasks.py
@@ -3,6 +3,7 @@ import os
 import json
 import random
 import time
+from datetime import datetime, timedelta, timezone
 import requests
 import numpy as np
 from sqlalchemy import desc, distinct
@@ -82,16 +83,6 @@ def import_analyze_store_list(list_data, org_id, user_email=None):
     mailing_list.calc_high_open_rate_pct()
     mailing_list.calc_cur_yr_stats()
 
-    # Create a list object
-    email_list = EmailList(
-        list_id=list_data['list_id'],
-        list_name=list_data['list_name'],
-        api_key=list_data['key'],
-        data_center=list_data['data_center'],
-        store_aggregates=list_data['store_aggregates'],
-        monthly_updates=list_data['monthly_updates'],
-        org_id=org_id)
-
     # Create a set of stats
     list_stats = ListStats( 
         frequency=mailing_list.frequency,
@@ -106,9 +97,20 @@ def import_analyze_store_list(list_data, org_id, user_email=None):
         cur_yr_inactive_pct=mailing_list.cur_yr_inactive_pct,
         list_id=list_data['list_id'])
 
-    # If the user gave their permission, store the object in the database
+    # If the user gave their permission, store the stats in the database
     if list_data['monthly_updates'] or list_data['store_aggregates']:
+
+        # Create a list object to go with the set of stats
+        email_list = EmailList(
+            list_id=list_data['list_id'],
+            list_name=list_data['list_name'],
+            api_key=list_data['key'],
+            data_center=list_data['data_center'],
+            store_aggregates=list_data['store_aggregates'],
+            monthly_updates=list_data['monthly_updates'],
+            org_id=org_id)
         email_list = db.session.merge(email_list)
+
         db.session.add(list_stats)
         try:
             db.session.commit()
@@ -116,7 +118,7 @@ def import_analyze_store_list(list_data, org_id, user_email=None):
             db.session.rollback()
             raise
 
-    return email_list
+    return list_stats
 
 def send_report(stats, list_id, list_name, user_email_or_emails):
     """Generates charts using Plotly and emails them to the user.
@@ -216,7 +218,7 @@ def send_report(stats, list_id, list_name, user_email_or_emails):
                    os.environ.get('SES_CONFIGURATION_SET') or None))
 
 def extract_stats(list_object):
-    """Extracts a stats dictionary from a list object from the database."""
+    """Extracts a stats dictionary from a SQLAlchemy ListStats object."""
     stats = {'subscribers': list_object.subscribers,
              'open_rate': list_object.open_rate,
              'hist_bin_counts': json.loads(list_object.hist_bin_counts),
@@ -232,9 +234,9 @@ def extract_stats(list_object):
 def init_list_analysis(user_data, list_data, org_id):
     """Celery task wrapper for each stage of analyzing a list.
 
-    First checks if the EmailList is cached, i.e. already in the
+    First checks if there is a recently cached analysis, i.e. already in the
     database. If not, calls import_analyze_store_list() to generate
-    the EmailList and an associated set of ListStats. Next updates the user's
+    the ListStats and an associated EmailList. Next updates the user's
     privacy options (e.g. store_aggregates, monthly_updates) if the list was
     cached. Then checks if the user selected monthly updates, if so,
     create the relationship. Finally, generates a benchmarking
@@ -246,79 +248,88 @@ def init_list_analysis(user_data, list_data, org_id):
         org_id: the id of the organization associated with the list.
     """
 
-    # Try to pull the EmailList from the database
-    # Otherwise generate it (and associated stats)
-    list_object = (EmailList.query.filter_by(
-        list_id=list_data['list_id']).first() or
-           import_analyze_store_list(
-               list_data, org_id, user_data['email']))
-
-    # Update the list privacy options if they differ from previous selection
-    if (list_object.monthly_updates != list_data['monthly_updates']
-        or list_object.store_aggregates != list_data['store_aggregates']):
-        list_object.monthly_updates = list_data['monthly_updates']
-        list_object.store_aggregates = list_data['store_aggregates']
-        list_object = db.session.merge(list_object)
-        try:
-            db.session.commit()
-        except:
-            db.session.rollback()
-            raise
-
-    # Associate the list with the user who requested the analysis
-    # If that user requested monthly updates
-    if list_data['monthly_updates']:
-        associate_user_with_list(user_data['user_id'], list_object)
-
-    # Grab the most recent set of stats for the given list
-    most_recent_analysis = ListStats.query.filter_by(
+    # Try to pull the most recent ListStats from the database
+    # Otherwise generate them
+    most_recent_analysis = (ListStats.query.filter_by(
         list_id=list_data['list_id']).order_by(desc(
-            'analysis_timestamp')).first()
+            'analysis_timestamp')).first() or import_analyze_store_list(
+                list_data, org_id, user_data['email']))
+
+    # If the user chose to store their data, there will be an associated
+    # EmailList object
+    list_object = EmailList.query.filter_by(
+        list_id=list_data['list_id']).first()
+
+    if list_object:
+
+        # Update the privacy options if they differ from previous selection
+        if (list_object.monthly_updates != list_data['monthly_updates']
+            or list_object.store_aggregates != list_data['store_aggregates']):
+            list_object.monthly_updates = list_data['monthly_updates']
+            list_object.store_aggregates = list_data['store_aggregates']
+            list_object = db.session.merge(list_object)
+            try:
+                db.session.commit()
+            except:
+                db.session.rollback()
+                raise
+
+        # Associate the list with the user who requested the analysis
+        # If that user requested monthly updates
+        if list_data['monthly_updates']:
+            associate_user_with_list(user_data['user_id'], list_object)
 
     # Convert the ListStats object to an easier-to-use dictionary
     stats = extract_stats(most_recent_analysis)
-
     send_report(stats, list_data['list_id'],
                 list_data['list_name'], [user_data['email']])
 
 @celery.task
 def update_stored_data():
     """Celery task which goes through the database
-    and updates calculations using the most recent data.
+    and generates a new set of calculations for each list older than 30 days.
 
     This task is called by Celery Beat, see the schedule in config.py.
     """
-
-    # Get the logger
     logger = get_task_logger(__name__)
 
-    # Grab what we have in the database
-    list_objects = EmailList.query.with_entities(
-        ListStats.list_id, ListStats.list_name, ListStats.org_id,
-        ListStats.api_key, ListStats.data_center,
-        ListStats.store_aggregates, ListStats.monthly_updates).all()
+    # Grab the most recent analyses in the database
+    list_analyses = ListStats.query.order_by(
+        'list_id', desc('analysis_timestamp')).distinct(
+            ListStats.list_id).all()
 
-    if not list_objects:
-        logger.info('No lists to update!')
+    if not list_analyses:
+        logger.warning('No lists in the database!')
+        return
+
+    # Create a list of analyses which are more than 30 days old
+    now = datetime.now(timezone.utc)
+    one_month_ago = now - timedelta(days=30)
+    analyses_to_update = [
+        analysis for analysis in list_analyses
+        if (analysis.analysis_timestamp.replace(
+            tzinfo=timezone.utc)) < one_month_ago]
+
+    if not analyses_to_update:
+        logger.info('No old lists to update!')
         return
 
     # Placeholder for lists which failed during the update process
     failed_updates = []
 
-    # Update 1/30th of the lists in the database (such that every list
-    # is updated about once per month, on average).
-    lists_to_update = random.sample(
-        list_objects, len(list_objects) // 31 if len(list_objects) // 31 else 1)
-
     # Update each list's calculations in sequence
-    for list_to_update in lists_to_update:
+    for analysis in analyses_to_update:
 
-        logger.info('Updating list %s!', list_to_update.list_id)
+        logger.info('Updating list %s!', analysis.list_id)
+
+        # Get the list object associated with the analysis
+        associated_list_object = analysis.list
 
         # Pull information about the list from the API
         # This may have changed since we originally pulled the list data
         request_uri = ('https://{}.api.mailchimp.com/3.0/lists/{}'.format(
-            list_to_update.data_center, list_to_update.list_id))
+            associated_list_object.data_center,
+            associated_list_object.list_id))
         params = (
             ('fields', 'stats.member_count,'
                        'stats.unsubscribe_count,'
@@ -329,7 +340,7 @@ def update_stored_data():
         )
         response = requests.get(
             request_uri, params=params,
-            auth=('shorenstein', list_to_update.api_key))
+            auth=('shorenstein', associated_list_object.api_key))
         response_body = response.json()
         response_stats = response_body['stats']
         count = (response_stats['member_count'] +
@@ -337,12 +348,12 @@ def update_stored_data():
                  response_stats['cleaned_count'])
 
         # Create a dictionary of list data
-        list_data = {'list_id': list_to_update.list_id,
-                     'list_name': list_to_update.list_name,
-                     'key': list_to_update.api_key,
-                     'data_center': list_to_update.data_center,
-                     'monthly_updates': list_to_update.monthly_updates,
-                     'store_aggregates': list_to_update.store_aggregates,
+        list_data = {'list_id': analysis.list_id,
+                     'list_name': associated_list_object.list_name,
+                     'key': associated_list_object.api_key,
+                     'data_center': associated_list_object.data_center,
+                     'monthly_updates': associated_list_object.monthly_updates,
+                     'store_aggregates': associated_list_object.store_aggregates,
                      'total_count': count,
                      'open_rate': response_stats['open_rate'],
                      'date_created': response_body['date_created'],
@@ -350,10 +361,10 @@ def update_stored_data():
 
         # Then re-run the calculations and update the database
         try:
-            import_analyze_store_list(list_data, list_to_update.org_id)
+            import_analyze_store_list(list_data, associated_list_object.org_id)
         except MailChimpImportError:
-            logger.error('Error updating list %s.', list_to_update.list_id)
-            failed_updates.append(list_to_update.list_id)
+            logger.error('Error updating list %s.', analysis.list_id)
+            failed_updates.append(analysis.list_id)
 
     # If any updates failed, raise an exception to send an error email
     if failed_updates:
@@ -371,7 +382,7 @@ def send_monthly_reports():
     logger = get_task_logger(__name__)
 
     # Grab info from the database
-    monthly_report_lists = ListStats.query.filter_by(
+    monthly_report_lists = EmailList.query.filter_by(
         monthly_updates=True).all()
 
     # Send an email report for each list
@@ -387,8 +398,13 @@ def send_monthly_reports():
                         monthly_report_list.list_name,
                         monthly_report_list.list_id)
 
+        # Get the most recent analysis for the list
+        stats_object = ListStats.query.filter_by(
+            list_id=monthly_report_list.list_id).order_by(
+                desc('analysis_timestamp')).first()
+
         # Extract stats from the list object
-        stats = extract_stats(monthly_report_list)
+        stats = extract_stats(stats_object)
         send_report(stats, monthly_report_list.list_id,
                     monthly_report_list.list_name,
                     users_to_email)
